bfb0841453d3fc9d9a8dcaf3392932e9eab78353
CamiloRamirezR <c.ramirezr2@uniandes.edu.co>
2024-10-03 16:17:58 -0500

    Se crea servicio para login

---
+++ src/app/user/user-service.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core'
+import { environment } from '../../environments/environment'
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserServiceService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/auth/employee`, { username, password });
+  }
+}
+
