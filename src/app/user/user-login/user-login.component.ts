629607080b83646dc748c2432c26b2ba4340d027
CamiloRamirezR <c.ramirezr2@uniandes.edu.co>
2024-10-04 17:31:05 +0200

    Implementación de servicio en vista

---
+++ src/app/user/user-login/user-login.component.ts
@@ -0,0 +1,43 @@
+import { NgClass } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { UserServiceService } from '../user-service.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [RouterLink, NgClass],
+  templateUrl: './user-login.component.html',
+  styleUrl: './user-login.component.css',
+})
+export class UserLoginComponent implements OnInit {
+  showPassword = false;
+  error: string = '';
+  helper = new JwtHelperService();
+
+  constructor(private userService: UserServiceService, private router: Router) {}
+
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  ngOnInit(): void {
+    sessionStorage.setItem('token', '');
+    sessionStorage.setItem('userId', '');
+  }
+
+  loginUser(username: string, password: string): void {
+    this.userService.login(username, password).subscribe(
+      (response) => {
+        sessionStorage.setItem('token', response.token);
+        const decodedToken = this.helper.decodeToken(response.token);
+        sessionStorage.setItem('userId', decodedToken.id);
+        this.router.navigate(['/alarms']);
+      },
+      (error) => {
+        this.error = 'Username or Password is incorrect';
+      }
+    );
+  }
+}
