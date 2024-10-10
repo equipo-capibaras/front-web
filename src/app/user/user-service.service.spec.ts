05238b20ee5e89f06eca222e5073e182e82e14eb
CamiloRamirezR <c.ramirezr2@uniandes.edu.co>
2024-10-09 19:00:18 -0500

    Pruebas unitarias en servicio

--- src/app/user/user-service.service.spec.ts
+++ src/app/user/user-service.service.spec.ts
@@ -45,4 +45,61 @@ describe('Service: UserService', () => {
     // Simula una respuesta exitosa del servidor
     req.flush(mockResponse);
   });
+
+  // Sin error
+  it('should handle login error', () => {
+    const mockErrorResponse = { status: 500, statusText: 'Internal Server Error' };
+    const username = 'testuser';
+    const password = 'password123';
+
+    service.login(username, password).subscribe(
+      () => fail('Debería haber fallado con un error 500'),
+      error => {
+        expect(error.message).toBe('Login failed. Please try again.');
+      },
+    );
+
+    const req = httpTestingController.expectOne(`${apiUrl}/auth/employee`);
+    expect(req.request.method).toBe('POST');
+
+    // Simula un error del servidor con un status 500
+    req.flush(null, mockErrorResponse);
+  });
+
+  it('should handle 404 Not Found error', () => {
+    const mockErrorResponse = { status: 404, statusText: 'Not Found' };
+    const username = 'testuser';
+    const password = 'password123';
+
+    service.login(username, password).subscribe(
+      () => fail('Debería haber fallado con un error 404'),
+      error => {
+        expect(error.message).toBe('Login failed. Please try again.');
+      },
+    );
+
+    const req = httpTestingController.expectOne(`${apiUrl}/auth/employee`);
+    expect(req.request.method).toBe('POST');
+
+    // Simula un error 404
+    req.flush(null, mockErrorResponse);
+  });
+
+  it('should handle empty or null response body', () => {
+    const username = 'testuser';
+    const password = 'password123';
+
+    service.login(username, password).subscribe(
+      response => {
+        expect(response).toBeNull(); // Verifica que la respuesta sea nula o indefinida
+      },
+      error => fail('No debería haber un error'),
+    );
+
+    const req = httpTestingController.expectOne(`${apiUrl}/auth/employee`);
+    expect(req.request.method).toBe('POST');
+
+    // Simula una respuesta con un cuerpo vacío
+    req.flush(null);
+  });
 });
