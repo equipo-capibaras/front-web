1809b93c68b7db2a59c37bcb1106f2703d6163e9
Laura Molina <ld.molina11@uniandes.edu.co>
2024-10-19 00:39:16 -0500

    :sparkles: Company Info

--- src/app/client/company-info/company-info.component.ts
+++ src/app/client/company-info/company-info.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-company-info',
   standalone: true,
   templateUrl: './company-info.component.html',
-  styleUrls: ['./company-info.component.css'],
+  styleUrls: ['./company-info.component.scss'],
+  imports: [MatIconModule],
 })
 export class CompanyInfoComponent implements OnInit {
   constructor() {}
