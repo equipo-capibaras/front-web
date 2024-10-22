f6c4e4a3c19c61b15bf07915924d0acb1f7f005c
Laura Molina <ld.molina11@uniandes.edu.co>
2024-10-22 18:52:56 -0500

    :sparkles: refactor snackbar in employee-register

--- src/app/pagination/pagination.ts
+++ src/app/pagination/pagination.ts
@@ -1,5 +1,7 @@
+import { Injectable } from '@angular/core';
 import { MatPaginatorIntl } from '@angular/material/paginator';
 
+@Injectable()
 export class CustomPaginatorIntl extends MatPaginatorIntl {
   override itemsPerPageLabel = $localize`:@@paginatorItemsPerPage:Items por página:`;
   override nextPageLabel = $localize`:@@paginatorNextPage:Siguiente página`;
