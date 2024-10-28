556213dc691de42598b6a842addff34f97840c19
Leidy Viviana Osorio <viviana@movingsuppliesusa.com>
2024-10-27 19:18:20 -0500

    Ajuste lint

--- src/app/employee/employee-unassigned/employee-unassigned.component.spec.ts
+++ src/app/employee/employee-unassigned/employee-unassigned.component.spec.ts
@@ -1,12 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { of, throwError } from 'rxjs';
+import { of } from 'rxjs';
 import { Router } from '@angular/router';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { AuthService } from 'src/app/auth/auth.service';
 import { ClientService } from 'src/app/client/client.service';
 import { EmployeeUnassignedComponent } from './employee-unassigned.component';
 import { InvitationDialogComponent } from '../invite-message/invite-message.component';
-import { Role } from 'src/app/auth/role';
 import { SnackbarService } from 'src/app/services/snackbar.service';
 import { DialogService } from 'src/app/client/invite-employee-dialog/dialog.services';
 import { InviteEmployeeDialogComponent } from 'src/app/client/invite-employee-dialog/invite-employee-dialog.component';
@@ -18,14 +17,12 @@ describe('EmployeeUnassignedComponent', () => {
   let component: EmployeeUnassignedComponent;
   let fixture: ComponentFixture<EmployeeUnassignedComponent>;
   let dialogSpy: jasmine.SpyObj<MatDialog>;
-  let clientServiceSpy: jasmine.SpyObj<ClientService>;
   let authServiceSpy: jasmine.SpyObj<AuthService>;
   let routerSpy: jasmine.SpyObj<Router>;
   let clientService: jasmine.SpyObj<ClientService>;
   let snackbarService: jasmine.SpyObj<SnackbarService>;
   let dialogRef: jasmine.SpyObj<MatDialogRef<InviteEmployeeDialogComponent>>;
   let dialogService: jasmine.SpyObj<DialogService>;
-  let authService: jasmine.SpyObj<AuthService>;
 
   beforeEach(() => {
     // Mocking services
@@ -34,10 +31,7 @@ describe('EmployeeUnassignedComponent', () => {
     dialogRef = jasmine.createSpyObj('MatDialogRef', ['close']);
     dialogService = jasmine.createSpyObj('DialogService', ['closeAllDialogs']);
     dialogSpy = jasmine.createSpyObj('MatDialog', ['open']);
-    clientServiceSpy = jasmine.createSpyObj('ClientService', [
-      'acceptInvitation',
-      'declineInvitation',
-    ]);
+
     authServiceSpy = jasmine.createSpyObj('AuthService', ['getToken', 'refreshToken', 'getRole']);
     routerSpy = jasmine.createSpyObj('Router', ['navigate']);
 
@@ -62,7 +56,6 @@ describe('EmployeeUnassignedComponent', () => {
     fixture = TestBed.createComponent(EmployeeUnassignedComponent);
     component = fixture.componentInstance;
     clientService = TestBed.inject(ClientService) as jasmine.SpyObj<ClientService>;
-    authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
 
     fixture.detectChanges();
 
